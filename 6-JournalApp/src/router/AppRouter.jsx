f073b42416aeadc5434d751c7e9d9d1c2695619f
Fernando Salamanca <lfernandosalamanca@gmail.com>
2023-04-04 21:15:57 +0200

    Reverted AppRouter.jsx to its original state

--- 6-JournalApp/src/router/AppRouter.jsx
+++ 6-JournalApp/src/router/AppRouter.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes';
 import { JournalRoutes } from '../journal/routes';
-import { LoginPage, RegisterPage } from '../auth/pages';
 
 export const AppRouter = () => {
     return (
-        <>
-            <Routes>
-                {/* Login && Register */}
-                <Route path="/auth/*" element={<AuthRoutes />}>
-                    <Route path="login" element={<LoginPage />} />
-                    <Route path="register" element={<RegisterPage />} />
-                </Route>
+        <Routes>
+            {/* Login && Register */}
+            <Route path="/auth/*" element={<AuthRoutes />} />
 
-                {/* JournalApp */}
-                <Route path="/*" element={<JournalRoutes />} />
-            </Routes>
-        </>
+            {/* JournalApp */}
+            <Route path="/*" element={<JournalRoutes />} />
+        </Routes>
     );
 };
